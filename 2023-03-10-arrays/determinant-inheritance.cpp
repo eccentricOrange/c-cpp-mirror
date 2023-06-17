24bf67eaf43baef036ffc9c5d03efcd0d7e6a49c
eccentricOrange <eccentric.orange2@gmail.com>
2023-06-17 11:11:22 +0000

    Used const, and renamed `matrix` --> `data` Fixes #4

--- 2023-03-10-arrays/determinant-inheritance.cpp
+++ 2023-03-10-arrays/determinant-inheritance.cpp
@@ -4,31 +4,31 @@
 template <std::size_t R, std::size_t C>
 class Matrix {
    protected:
-    std::array<std::array<double, C>, R> matrix;
+    std::array<std::array<double, C>, R> data;
 
    public:
     /// @brief sets all elements of the matrix to 0
     Matrix() {
         for (std::size_t row = 0; row < R; row++) {
             for (std::size_t column = 0; column < C; column++) {
-                matrix[row][column] = 0;
+                data[row][column] = 0;
             }
         }
     }
 
     /// @brief sets the matrix to the given matrix
     /// @param matrix the input data
-    Matrix(std::array<std::array<double, C>, R> matrix) : matrix(matrix) {}
+    Matrix(std::array<std::array<double, C>, R> matrix) : data(matrix) {}
 
     /// @brief adds the given matrix to the current matrix, element by element
     /// @param matrix the input data
     /// @return the sum of the two matrices
-    Matrix<R, C> operator+(Matrix<R, C> matrix) {
+    Matrix<R, C> operator+(const Matrix<R, C>& addend) {
         Matrix<R, C> returnMatrix;
 
         for (std::size_t row = 0; row < R; row++) {
             for (std::size_t column = 0; column < C; column++) {
-                returnMatrix[row][column] = this->matrix[row][column] + matrix[row][column];
+                returnMatrix[row][column] = data[row][column] + addend.data[row][column];
             }
         }
 
@@ -38,12 +38,12 @@ class Matrix {
     /// @brief subtracts the given matrix from the current matrix, element by element
     /// @param matrix the input data
     /// @return the difference of the two matrices
-    Matrix<R, C> operator-(Matrix<R, C> matrix) {
+    Matrix<R, C> operator-(Matrix<R, C> subtrahend) {
         Matrix<R, C> returnMatrix;
 
         for (std::size_t row = 0; row < R; row++) {
             for (std::size_t column = 0; column < C; column++) {
-                returnMatrix[row][column] = this->matrix[row][column] - matrix[row][column];
+                returnMatrix[row][column] = data[row][column] - subtrahend.data[row][column];
             }
         }
 
@@ -54,13 +54,13 @@ class Matrix {
     /// @param matrix the input data
     /// @return the product of the two matrices
     /// @warning the number of columns of the first matrix must be equal to the number of rows of the second matrix. I'm unable to figure out a way to check this, since the number of rows and columns are template parameters
-    Matrix<R, C> operator*(Matrix<R, C> matrix) {
+    Matrix<R, C> operator*(Matrix<R, C> factor) {
         Matrix<R, C> returnMatrix;
 
         for (std::size_t k = 0; k < R; k++) {
             for (std::size_t i = 0; i < R; i++) {
                 for (std::size_t j = 0; j < C; j++) {
-                    returnMatrix[i][j] += this->matrix[i][k] * matrix[k][j];
+                    returnMatrix[i][j] += data[i][k] * factor.data[k][j];
                 }
             }
         }
@@ -72,42 +72,42 @@ class Matrix {
     /// @param row index of the row
     /// @return the row of the matrix, as an array
     std::array<double, C>& operator[](std::size_t row) {
-        return matrix[row];
+        return data[row];
     }
 
     /// @brief return the matrix
     /// @return the `std::array<std::array>` object
     std::array<std::array<double, C>, R> getMatrix() {
-        return matrix;
+        return data;
     }
 
     /// @brief returns the element at the given row and column
     /// @param row index of the row
     /// @param column index of the column
     int getElement(std::size_t row, std::size_t column) {
-        return matrix[row][column];
+        return data[row][column];
     }
 
     /// @brief sets the matrix to the given matrix
     /// @param matrix the input data
-    void setMatrix(std::array<std::array<double, C>, R> matrix) {
-        this->matrix = matrix;
+    void setMatrix(std::array<std::array<double, C>, R> data) {
+        this->data = data;
     }
 
     /// @brief sets the element at the given row and column to the given value
     /// @param row index of the row
     /// @param column index of the column
     void setElement(std::size_t row, std::size_t column, double value) {
-        matrix[row][column] = value;
+        data[row][column] = value;
     }
 
     /// @brief prints the matrix to the given output stream
-    friend std::ostream& operator<<(std::ostream& out, Matrix<R, C> matrix) {
+    friend std::ostream& operator<<(std::ostream& out, const Matrix<R, C>& matrix) {
         out << "[" << R << 'x' << C << "]\n";
 
         for (std::size_t row = 0; row < R; row++) {
             for (std::size_t column = 0; column < C; column++) {
-                out << matrix[row][column] << '\t';
+                out << matrix.data[row][column] << '\t';
             }
 
             out << '\n';
@@ -129,7 +129,7 @@ class SquareMatrix : public Matrix<N, N> {
 
     /// @brief sets the matrix to the given matrix. reuses the constructor
     /// @param matrix  the input data
-    SquareMatrix(std::array<std::array<double, N>, N> matrix) : Matrix<N, N>(matrix) {}
+    SquareMatrix(std::array<std::array<double, N>, N> data) : Matrix<N, N>(data) {}
 
     /// @brief extracts the submatrix of the given matrix, ignoring the given row and column
     /// @param ignoreRow row to ignore
@@ -143,7 +143,7 @@ class SquareMatrix : public Matrix<N, N> {
         for (std::size_t matrixRow = 0; matrixRow < N; matrixRow++) {
             for (std::size_t matrixColumn = 0; matrixColumn < N; matrixColumn++) {
                 if (matrixRow != ignoreRow && matrixColumn != ignoreColumn) {
-                    returnMatrix[subMatrixRow][subMatrixColumn++] = this->matrix[matrixRow][matrixColumn];
+                    returnMatrix[subMatrixRow][subMatrixColumn++] = this->data[matrixRow][matrixColumn];
 
                     if (subMatrixColumn == N - 1) {
                         subMatrixColumn = 0;
@@ -164,12 +164,12 @@ class SquareMatrix : public Matrix<N, N> {
 
         // base case 1
         if constexpr (N == 1) {
-            determinant = this->matrix[0][0];
+            determinant = this->data[0][0];
         }
 
         // base case 2
         else if constexpr (N == 2) {
-            determinant = (this->matrix[0][0] * this->matrix[1][1]) - (this->matrix[0][1] * this->matrix[1][0]);
+            determinant = (this->data[0][0] * this->data[1][1]) - (this->data[0][1] * this->data[1][0]);
         }
 
         // recursive case
@@ -178,7 +178,7 @@ class SquareMatrix : public Matrix<N, N> {
 
             for (std::size_t dimension = 0; dimension < N; dimension++) {
                 // calculate cofactor and add to determinant
-                determinant += sign * this->matrix[0][dimension] * subMatrix(0, dimension).getDeterminant();
+                determinant += sign * this->data[0][dimension] * subMatrix(0, dimension).getDeterminant();
                 sign = -sign;
             }
         }
