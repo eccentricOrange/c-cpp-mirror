af6bb7c0e0fa82934f3e49a306102be127dcf692
eccentricOrange <eccentric.orange2@gmail.com>
2023-06-24 11:28:34 +0000

    random optimizations

--- 2023-06-22-determinant-inheritance/matrix.h
+++ 2023-06-22-determinant-inheritance/matrix.h
@@ -12,36 +12,34 @@ class Matrix {
 
    public:
     Matrix() {
-        for (std::size_t row = 0; row < R; row++) {
-            for (std::size_t column = 0; column < C; column++) {
-                data[row][column] = 0;
-            }
+        for (auto& row : data) {
+            std::fill(row.begin(), row.end(), 0);
         }
     }
 
     Matrix(std::array<std::array<double, C>, R> matrix) : data(matrix) {}
 
-    constexpr std::array<double, C>& operator[](std::size_t row) {
+    constexpr std::array<double, C>& operator[](const std::size_t row) {
         return data[row];
     }
 
-    constexpr const std::array<double, C>& operator[](std::size_t row) const {
+    constexpr const std::array<double, C>& operator[](const std::size_t row) const {
         return data[row];
     }
 
-    constexpr std::array<double, C>& at(std::size_t row) {
+    constexpr std::array<double, C>& at(const std::size_t row) {
         return data.at(row);
     }
 
-    constexpr const std::array<double, C>& at(std::size_t row) const {
+    constexpr const std::array<double, C>& at(const std::size_t row) const {
         return data.at(row);
     }
 
-    constexpr double& at(std::size_t row, std::size_t column) {
+    constexpr double& at(const std::size_t row, const std::size_t column) {
         return data.at(row).at(column);
     }
 
-    constexpr const double& at(std::size_t row, std::size_t column) const {
+    constexpr const double& at(const std::size_t row, const std::size_t column) const {
         return data.at(row).at(column);
     }
 
@@ -57,7 +55,7 @@ class Matrix {
         return returnMatrix;
     }
 
-    friend Matrix operator+(double lhs, const Matrix& rhs) {
+    friend Matrix operator+(const double lhs, const Matrix& rhs) {
         Matrix returnMatrix;
 
         for (std::size_t row = 0; row < R; row++) {
@@ -69,7 +67,7 @@ class Matrix {
         return returnMatrix;
     }
 
-    friend Matrix operator+(const Matrix& lhs, double rhs) {
+    friend Matrix operator+(const Matrix& lhs, const double rhs) {
         return rhs + lhs;
     }
 
@@ -85,7 +83,7 @@ class Matrix {
         return returnMatrix;
     }
 
-    friend Matrix operator-(double lhs, const Matrix& rhs) {
+    friend Matrix operator-(const double lhs, const Matrix& rhs) {
         Matrix returnMatrix;
 
         for (std::size_t row = 0; row < R; row++) {
@@ -97,7 +95,7 @@ class Matrix {
         return returnMatrix;
     }
 
-    friend Matrix operator-(const Matrix& lhs, double rhs) {
+    friend Matrix operator-(const Matrix& lhs, const double rhs) {
         Matrix returnMatrix;
 
         for (std::size_t row = 0; row < R; row++) {
@@ -109,23 +107,10 @@ class Matrix {
         return returnMatrix;
     }
 
-    friend Matrix operator*(const Matrix& lhs, const Matrix& rhs) {
-        Matrix returnMatrix;
+    template <std::size_t T, std::size_t U, std::size_t V>
+    friend Matrix<T, V> operator*(const Matrix<T, U>& lhs, const Matrix<U, V>& rhs);
 
-        for (std::size_t row = 0; row < R; row++) {
-            for (std::size_t column = 0; column < C; column++) {
-                returnMatrix.data[row][column] = 0;
-
-                for (std::size_t i = 0; i < C; i++) {
-                    returnMatrix.data[row][column] += lhs.data[row][i] * rhs.data[i][column];
-                }
-            }
-        }
-
-        return returnMatrix;
-    }
-
-    friend Matrix operator*(double lhs, const Matrix& rhs) {
+    friend Matrix operator*(const double lhs, const Matrix& rhs) {
         Matrix returnMatrix;
 
         for (std::size_t row = 0; row < R; row++) {
@@ -137,16 +122,16 @@ class Matrix {
         return returnMatrix;
     }
 
-    friend Matrix operator*(const Matrix& lhs, double rhs) {
+    friend Matrix operator*(const Matrix& lhs, const double rhs) {
         return rhs * lhs;
     }
 
     friend std::ostream& operator<<(std::ostream& out, const Matrix& matrix) {
         out << "[" << R << 'x' << C << "]\n";
 
-        for (std::size_t row = 0; row < R; row++) {
-            for (std::size_t column = 0; column < C; column++) {
-                out << matrix.data[row][column] << '\t';
+        for (const auto row : matrix.data) {
+            for (const auto element : row) {
+                out << element << '\t';
             }
 
             out << '\n';
@@ -158,17 +143,17 @@ class Matrix {
     }
 
     template <std::size_t N>
-    friend double getDeterminant(Matrix<N, N> matrix);
+    friend double const getDeterminant(const Matrix<N, N>& matrix);
 
     template <std::size_t N>
-    friend Matrix<N - 1, N - 1> subMatrix(Matrix<N, N> matrix, std::size_t ignoreRow, std::size_t ignoreColumn);
+    friend Matrix<N - 1, N - 1> subMatrix(const Matrix<N, N>& matrix, const std::size_t ignoreRow, const std::size_t ignoreColumn);
 };
 
 template <std::size_t N>
 using SquareMatrix = Matrix<N, N>;
 
 template <std::size_t N>
-SquareMatrix<N - 1> subMatrix(SquareMatrix<N> matrix, std::size_t ignoreRow, std::size_t ignoreColumn) {
+SquareMatrix<N - 1> subMatrix(const SquareMatrix<N>& matrix, const std::size_t ignoreRow, const std::size_t ignoreColumn) {
     SquareMatrix<N - 1> returnMatrix;
 
     int subMatrixRow = 0, subMatrixColumn = 0;
@@ -190,13 +175,11 @@ SquareMatrix<N - 1> subMatrix(SquareMatrix<N> matrix, std::size_t ignoreRow, std
 }
 
 template <std::size_t N>
-double getDeterminant(SquareMatrix<N> matrix) {
+double const getDeterminant(const SquareMatrix<N>& matrix) {
     double determinant = 0;
 
     if constexpr (N == 1) {
         determinant = matrix[0][0];
-    } else if constexpr (N == 2) {
-        determinant = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
     } else if constexpr (N > 0) {
         for (std::size_t column = 0; column < N; column++) {
             determinant += std::pow(-1, column) * matrix[0][column] * getDeterminant(subMatrix(matrix, 0, column));
@@ -206,6 +189,21 @@ double getDeterminant(SquareMatrix<N> matrix) {
     }
 
     return determinant;
+}
+
+template <std::size_t R, std::size_t RHC, std::size_t C>
+Matrix<R, C> operator*(const Matrix<R, RHC>& lhs, const Matrix<RHC, C>& rhs) {
+    Matrix<R, C> returnMatrix;
+
+    for (std::size_t row = 0; row < R; row++) {
+        for (std::size_t column = 0; column < C; column++) {
+            for (std::size_t i = 0; i < RHC; i++) {
+                returnMatrix[row][column] += lhs.data[row][i] * rhs.data[i][column];
+            }
+        }
+    }
+
+    return returnMatrix;
 }
 
 #endif  // DETERMINANT_INHERITANCE_H
